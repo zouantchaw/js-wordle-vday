8925c43597f2d9bf5f94650bbe388360ba1bd76d
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2022-02-11 16:53:44 -0500

    style key buttons

--- app.js
+++ app.js
@@ -32,9 +32,16 @@ const keys = [
   '«',
 ]
 
+const handleClick = () => {
+  console.log('clicked')
+
+}
+
 keys.forEach(key => {
   const buttonElement = document.createElement('button');
   buttonElement.textContent = key
+  buttonElement.setAttribute('id', key)
   console.log(buttonElement)
+  buttonElement.addEventListener('click', handleClick)
   keyboard.append(buttonElement)
 })
